87296235a4a4dcf8469cadc03dffb926d0396019
Darren Birkett <darren.birkett@gmail.com>
2013-06-04 13:01:30 +0100

    fix docstrings

--- crap/task.py
+++ crap/task.py
@@ -1,13 +1,13 @@
 from crap.commands import BaseShowCommand, BaseListCommand
 
 class Show(BaseShowCommand):
-    "Show details about a task"
+    "Show details about a single task"
 
     def __init__(self, app, app_args):
         super(Show, self).__init__(app, app_args, artifact_type='Task')
 
 class List(BaseListCommand):
-    "Show details about a task"
+    "Get a list of tasks"
 
     def __init__(self, app, app_args):
         super(List, self).__init__(app, app_args, artifact_type='Task')
